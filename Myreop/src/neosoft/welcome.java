b9019f079781bce1cba4a452540ea5b5862abcca
Imtiyaz16 <jiwaniimtiyaaz@gmail.com>
2021-09-16 11:48:12 +0530

    Initial commit

---
+++ Myreop/src/neosoft/welcome.java
@@ -0,0 +1,7 @@
+package neosoft;
+
+public class welcome {
+	public static void main(String args[]) {
+		System.out.println("Welcome");
+	}
+}
---
+++ Myreop/src/testing/maincalss.java
@@ -0,0 +1,7 @@
+package testing;
+
+public class maincalss {
+	public static void main(String args[]){
+		System.out.println("welcome");
+	}
+}
